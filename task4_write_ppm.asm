4ce720d9d97bddfad6008c02f73453d143998f2d
Rorisang Manamela <114072358+RorisangManamela@users.noreply.github.com>
2024-10-26 20:43:34 +0200

    Updated task 4, not complete

--- task4_write_ppm.asm
+++ task4_write_ppm.asm
@@ -10,4 +10,30 @@ section .text
     global writePPM
 
 writePPM:
+    ; Allocate space on the stack
+    sub rsp, 40                     
+
+    ; 1. Open the File: Open the file for reading
+    mov rdi, rdi                        ; Filename is passed in the first argument (rdi) - (Input 1)
+    mov rsi, mode_str                   ; Mode = "r"
+    call fopen                          ; fmopen is a function for opening files
+
+    test rax, rax                       ; Check if file opened successfully
+    jz .error                           ; If it failed, handle it
+    mov rbx, rax                        ; Save file pointer
+
+
+    mov rdi, [rdi]                      ; Load head of linked list (PixelNode* head) - (Input 2)
+    cmp rdi, 0                          ; Check if the node is NULL
+    je  compute_cdf                     ; Jump to CDF computation if NULL
+
+
+.error:
+    mov rax, 0                          ; Return null in case of error
+    add rsp, 40                         ; Restore stack
+    ret
+
+
+
+
     
